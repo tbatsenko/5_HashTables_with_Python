ed6c0fe029ac4fc6af76b67c987f41783f946f2c
Tanya Batsenko <tanyabatsenko@Tanyas-MacBook-Air.local>
2017-11-07 21:53:36 +0200

    implement all

--- task_05.py
+++ task_05.py
@@ -5,87 +5,7 @@ class Node:
         self.value = value
         self.next_node = next_node
 
-
-class ChainedHashMultiplication(BasicHashTable):
-    def hash(self, key):
-        A = 0.6180339887
-        return len(self.size*(key*A % 1)
-
-    def __init__(self, hash_type, values):
-        super().__init__(self, hash_type, values)
-
-    def add_element(self, num):
-        curr_node = self.hash_table[self.hash(num)]
-        if curr_node:
-            while curr_node.next:
-                curr_node = curr_node.next
-            curr_node.next = Node(num)
-        else:
-            self.hash_table[self.hash(num)] = num
-
-    def get_element(self, num):
-        curr_node = self.hash_table[self.hash(num)]
-        if curr_node:
-            while curr_node.next:
-                if curr_node.value == num:
-                    return True
-                curr_node = curr_node.next
-        return False
-
-
-
-class ChainedHashDivision(BasicHashTable):
-    def hash(self, key):
-        return key % self.size
-
-    def __init__(self, hash_type, values):
-        super().__init__(self, hash_type, values)
-
-    def add_element(self, num):
-        curr_node = self.hash_table[self.hash(num)]
-        if curr_node:
-            while curr_node.next:
-                curr_node = curr_node.next
-            curr_node.next = Node(num)
-        else:
-            self.hash_table[self.hash(num)] = num
-
-    def get_element(self, num):
-        curr_node = self.hash_table[self.hash(num)]
-        if curr_node:
-            while curr_node.next:
-                if curr_node.value == num:
-                    return True
-                curr_node = curr_node.next
-        return False
-
-
-class OpenAdressHashLinear(BasicHashTable):
-    def __init__(self, hash_type, values):
-        super().__init__(self, hash_type, values)
-
-    def hash(key, i):
-        return (ChainedHashDivision.hash(key) + i) % self.size
-
-
-class OpenAdressHashSquare(BasicHashTable):
-    def __init__(self, hash_type, values):
-        super().__init__(self, hash_type, values)
-
-    def hash(key, i):
-        return (ChainedHashDivision.hash(key) + i*2 + i**2 * 3) % self.size
-
-
-class OpenAdressHashDouble(BasicHashTable):
-    super().__init__(self, hash_type, values)
-        super().__init__(self, values)
-
-    def hash(key, i):
-        h1 = lambda x: x % self.size
-        return (h1(key) + i*ChainedHashMultiplication.hash(key)) % self.size
-
-
-class HashTable:
+class BasicHashTable:
     def __init__(self, hash_type, values):
         self.values = values
 
@@ -109,48 +29,113 @@ class HashTable:
 
         self.hash_table = [None for el in range(self.size)]
 
-        for num in values:
-            if self.hash_type == 1:
-                curr_index = ChainedHashDivision.hash(num)
 
-            elif self.hash_type == 2:
-                curr_index = ChainedHashMultiplication.hash(num)
+class ChainedHash(BasicHashTable):
+    def __init__(self, hash_type, values):
+        super().__init__(self, hash_type, values)
+        self.hash = lambda key: key % self.size
 
-            elif self.hash_type == 3:
-                curr_index = OpenAdressHashLinear.hash(num)
-
-            elif self.hash_type == 4:
-                curr_index = OpenAdressHashSquare.hash(num)
-
-            else:
-                curr_index = OpenAdressHashDouble.hash(num)
-
-            self.hash_table[curr_index] = num
+    def add_element(self, num):
+        curr_node = self.hash_table[self.hash(num)]
+        if curr_node:
+            while curr_node.next:
+                curr_node = curr_node.next
+            curr_node.next = Node(num)
+        else:
+            self.hash_table[self.hash(num)] = num
 
     def get_element(self, num):
-        if self.hash_type == 1:
-            if self.hash_table[ChainedHashDivision.hash(num)]:
-                if self.hash_table[ChainedHashDivision.hash(num)] == num:
+        curr_node = self.hash_table[self.hash(num)]
+        if curr_node:
+            while curr_node.next:
+                if curr_node.value == num:
                     return True
-                else:
-                    while condition:
-                        pass
+                curr_node = curr_node.next
+        return False
+
+
+class ChainedHashMultiply(ChainedHash):
+    def __init__(self, hash_type, values):
+        super().__init__(self, hash_type, values)
+        A = 0.6180339887
+        self.hash = lambda key : len(self.size*(key*A % 1)
+
+
+class OpenAdressHash(BasicHashTable):
+    def __init__(self, hash_type, values):
+        super().__init__(self, hash_type, values)
+        self.hash = lambda key, i: (ChainedHash.hash(key) + i) % self.size
+
+    def add_element(self, num):
+        start_index = self.hash(num)
+        curr_node = self.hash_table[start_index]
+        if curr_node != None:
+            i = 0
+            while self.hash_table[start_index + i] != None:
+                i += 1
+            self.hash_table[start_index + i] = num
+        else:
+            self.hash_table[start_index] = num
+
+    def get_element(self, num):
+        curr_node = self.hash_table[self.hash(num)]
+        if curr_node:
+            while curr_node.next:
+                if curr_node.value == num:
+                    return True
+                curr_node = curr_node.next
+        return False
+
+        start_index = self.hash(num)
+        curr_node = self.hash_table[start_index]
+        if curr_node != None:
+            i = 0
+            while self.hash_table[start_index + i] != num:
+                if self.hash_table[start_index + i] == None:
+                    return False
+                i += 1
+            return True
+        else:
+            return False
+
+
+class OpenAdressHashSquare(OpenAdressHash):
+    def __init__(self, hash_type, values):
+        super().__init__(self, hash_type, values)
+
+    def hash(key, i):
+        return (ChainedHashDivision.hash(key) + i*2 + i**2 * 3) % self.size
+
+
+class OpenAdressHashDouble(OpenAdressHash):
+    def __init__(self, hash_type, values):
+        super().__init__(self, hash_type, values)
+
+    def hash(key, i):
+        h1 = lambda x: x % self.size
+        return (h1(key) + i*ChainedHashMultiply.hash(key)) % self.size
+    
+
+class HashTable:
+    def __init__(self, hash_type, values):
+        if self.hash_type == 1:
+            self.current_hash_table = ChainedHash(hash_type, values)
 
         elif self.hash_type == 2:
-            curr_index = ChainedHashMultiplication.hash(num)
+            self.current_hash_table = ChainedHashMultiply(hash_type, values)
 
         elif self.hash_type == 3:
-            curr_index = OpenAdressHashLinear.hash(num)
+            self.current_hash_table = OpenAdressHash(hash_type, values)
 
         elif self.hash_type == 4:
-            curr_index = OpenAdressHashSquare.hash(num)
+            self.current_hash_table = OpenAdressHashSquare(hash_type, values)
 
         else:
-            curr_index = OpenAdressHashDouble.hash(num)
+            self.current_hash_table = OpenAdressHashDouble(hash_type, values)
 
 
-
-
+    def get_element(self, num):
+        return self.current_hash_table.get_element(num)
 
 
     def get_collisions_amount(self):
@@ -161,9 +146,13 @@ class HashTable:
         return nones_amount + len(self.values) - self.size
 
     def find_sum(s):
+        res = []
         for x in range(s // 2):
             y = s - x
-            if
+            if self.current_hash_table.get_element(x) and self.current_hash_table.get_element(y):
+                res.append(x, y)
+
+        return res
 
 
 
